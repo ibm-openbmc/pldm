44982b707c2d8e2afbe252036439e214b76285d6
Sagar Srinivas <sagar.srinivas@ibm.com>
2021-08-16 12:27:50 -0500

    oem-ibm: Eliminate the Boot side Effecters and sensors
    This commit is to eliminate the bootside sensors and
    effecters which was used during the Inband Code Update.
    
    The Host will be making use of the bios attribute
    introduced to reflect the bootside the host is running
    on in https://gerrit.openbmc-project.xyz/c/openbmc/pldm/+/45387/3
    
    Signed-off-by: Sagar Srinivas <sagar.srinivas@ibm.com>
    Change-Id: I4212d6cb482f3b596c419988f6a0a17cc543d5f5

--- oem/ibm/libpldmresponder/oem_ibm_handler.cpp
+++ oem/ibm/libpldmresponder/oem_ibm_handler.cpp
@@ -78,7 +78,7 @@ int pldm::responder::oem_ibm_platform::Handler::
 
 int pldm::responder::oem_ibm_platform::Handler::
     oemSetStateEffecterStatesHandler(
-        uint16_t entityType, uint16_t entityInstance, uint16_t stateSetId,
+        uint16_t entityType, uint16_t /*entityInstance*/, uint16_t stateSetId,
         uint8_t compEffecterCnt,
         std::vector<set_effecter_state_field>& stateField, uint16_t effecterId)
 {
@@ -90,13 +90,7 @@ int pldm::responder::oem_ibm_platform::Handler::
         if (stateField[currState].set_request == PLDM_REQUEST_SET)
         {
             if (entityType == PLDM_OEM_IBM_ENTITY_FIRMWARE_UPDATE &&
-                stateSetId == PLDM_OEM_IBM_BOOT_STATE)
-            {
-                rc = setBootSide(entityInstance, currState, stateField,
-                                 codeUpdate);
-            }
-            else if (entityType == PLDM_OEM_IBM_ENTITY_FIRMWARE_UPDATE &&
-                     stateSetId == PLDM_OEM_IBM_FIRMWARE_UPDATE_STATE)
+                stateSetId == PLDM_OEM_IBM_FIRMWARE_UPDATE_STATE)
             {
                 if (stateField[currState].effecter_state ==
                     uint8_t(CodeUpdateState::START))
@@ -353,8 +347,7 @@ void buildAllCodeUpdateEffecterPDR(oem_ibm_platform::Handler* platformHandler,
     possibleStates->possible_states_size = 2;
     auto state =
         reinterpret_cast<state_effecter_possible_states*>(possibleStates);
-    if ((stateSetID == PLDM_OEM_IBM_BOOT_STATE) ||
-        (stateSetID == PLDM_OEM_IBM_BOOT_SIDE_RENAME))
+    if (stateSetID == PLDM_OEM_IBM_BOOT_SIDE_RENAME)
         state->states[0].byte = 6;
     else if (stateSetID == PLDM_OEM_IBM_FIRMWARE_UPDATE_STATE)
         state->states[0].byte = 126;
@@ -470,8 +463,7 @@ void buildAllCodeUpdateSensorPDR(oem_ibm_platform::Handler* platformHandler,
     possibleStates->possible_states_size = 2;
     auto state =
         reinterpret_cast<state_sensor_possible_states*>(possibleStates);
-    if ((stateSetID == PLDM_OEM_IBM_BOOT_STATE) ||
-        (stateSetID == PLDM_OEM_IBM_VERIFICATION_STATE) ||
+    if ((stateSetID == PLDM_OEM_IBM_VERIFICATION_STATE) ||
         (stateSetID == PLDM_OEM_IBM_BOOT_SIDE_RENAME))
         state->states[0].byte = 6;
     else if (stateSetID == PLDM_OEM_IBM_FIRMWARE_UPDATE_STATE)
@@ -641,12 +633,6 @@ void pldm::responder::oem_ibm_platform::Handler::buildOEMPDR(
     pdr_utils::Repo& repo)
 {
     buildAllCodeUpdateEffecterPDR(this, PLDM_OEM_IBM_ENTITY_FIRMWARE_UPDATE,
-                                  ENTITY_INSTANCE_0, PLDM_OEM_IBM_BOOT_STATE,
-                                  repo);
-    buildAllCodeUpdateEffecterPDR(this, PLDM_OEM_IBM_ENTITY_FIRMWARE_UPDATE,
-                                  ENTITY_INSTANCE_1, PLDM_OEM_IBM_BOOT_STATE,
-                                  repo);
-    buildAllCodeUpdateEffecterPDR(this, PLDM_OEM_IBM_ENTITY_FIRMWARE_UPDATE,
                                   ENTITY_INSTANCE_0,
                                   PLDM_OEM_IBM_FIRMWARE_UPDATE_STATE, repo);
     buildAllCodeUpdateEffecterPDR(this, PLDM_ENTITY_SYSTEM_CHASSIS,
@@ -664,12 +650,6 @@ void pldm::responder::oem_ibm_platform::Handler::buildOEMPDR(
     auto slotPaths = dBusIntf->getSubTreePaths(objectPath, 0, slotInterface);
     buildAllSlotEnableEffecterPDR(this, repo, slotPaths);
     buildAllSlotEnableSensorPDR(this, repo, slotPaths);
-    buildAllCodeUpdateSensorPDR(this, PLDM_OEM_IBM_ENTITY_FIRMWARE_UPDATE,
-                                ENTITY_INSTANCE_0, PLDM_OEM_IBM_BOOT_STATE,
-                                repo);
-    buildAllCodeUpdateSensorPDR(this, PLDM_OEM_IBM_ENTITY_FIRMWARE_UPDATE,
-                                ENTITY_INSTANCE_1, PLDM_OEM_IBM_BOOT_STATE,
-                                repo);
     buildAllCodeUpdateSensorPDR(this, PLDM_OEM_IBM_ENTITY_FIRMWARE_UPDATE,
                                 ENTITY_INSTANCE_0,
                                 PLDM_OEM_IBM_FIRMWARE_UPDATE_STATE, repo);
