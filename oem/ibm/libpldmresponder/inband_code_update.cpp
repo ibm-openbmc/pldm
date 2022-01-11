b833f7d50c81243cebbb1377c8f0139bd1db4597
Sagar Srinivas <sagar.srinivas@ibm.com>
2022-01-11 09:09:35 -0600

    PLDM Fix for bootside attribute defect
    SW540350
    
    Signed-off-by: Sagar Srinivas <sagar.srinivas@ibm.com>

--- oem/ibm/libpldmresponder/inband_code_update.cpp
+++ oem/ibm/libpldmresponder/inband_code_update.cpp
@@ -213,8 +213,10 @@ void CodeUpdate::setVersions()
             pldm_boot_side_data pldmBootSideData = readBootSideFile();
             if (pldmBootSideData.running_version_object != runningVersion)
             {
-                pldmBootSideData.current_boot_side = "Temp" ? "Perm" : "Temp";
-                pldmBootSideData.next_boot_side = "Temp" ? "Perm" : "Temp";
+                auto current_boot_side = (pldmBootSideData.current_boot_side == "Temp" ? "Perm" : "Temp");
+                pldmBootSideData.current_boot_side = current_boot_side;
+                auto next_boot_side = (pldmBootSideData.next_boot_side == "Temp" ? "Perm" : "Temp");
+                pldmBootSideData.next_boot_side = next_boot_side;
                 writeBootSideFile(pldmBootSideData);
                 biosAttrList.push_back(
                     std::make_pair((std::string)bootSideAttrName,
