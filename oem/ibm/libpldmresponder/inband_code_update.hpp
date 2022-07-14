f87cee04c1dc9f397332823c9bfcd6025308de41
sagisin <65535468+sagisin@users.noreply.github.com>
2022-07-14 21:49:06 +0530

    PLDM:Wait on the Software Updater (#296)
    Wait on the Software Updater before getting the Association endpoints,
    during the code update as there maybe delays in BMC software
    updater.
    
    Defect:SW554311
    
    Signed-off-by: Sagar Srinivas <sagar.srinivas@ibm.com>

--- oem/ibm/libpldmresponder/inband_code_update.hpp
+++ oem/ibm/libpldmresponder/inband_code_update.hpp
@@ -13,6 +13,9 @@ namespace responder
 
 static constexpr uint8_t pSideNum = 1;
 static constexpr uint8_t tSideNum = 2;
+/** Wait on the Software Updater as the code update may take longer
+ * and hence there may be delays in BMC software update**/
+static constexpr uint8_t maxVersionRetry = 20;
 static constexpr auto Pside = "P";
 static constexpr auto Tside = "T";
 
@@ -233,6 +236,8 @@ class CodeUpdate
     std::string runningVersion;    //!< currently running image
     std::string nonRunningVersion; //!< alternate image
     std::string newImageId;        //!< new image id
+    uint8_t retrySetVersion = 0;
+
     bool codeUpdateInProgress =
         false; //!< indicates whether codeupdate is going on
     bool outOfBandCodeUpdateInProgress = false; //!< indicates whether
