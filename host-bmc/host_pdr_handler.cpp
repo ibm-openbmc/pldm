ibm-openbmc/pldm

commit 9d07afa86285860c2c8f811fda712d4bf471373f

PLDM: Changes to persist the HB PDRs in the repo before merging (#319)
This commit persists the HB range PDRs in the repo before
they are merged. This change was needed as PHYP would not send
down BMC the entity Association PDRs of HB after a R/R as the
PDRs will be in BMC range of record handles. Due to this
the pldm ebmcModel in the PHYP was not updated properly.

This commit also prevents adding duplicate terminus locator
PDRs to the repo.

Tested: Multiple poweron and poweroff
Multiple Reset Reloads

Fixes: SW553100

Signed-off-by: Pavithra Barithaya <pavithra.b@ibm.com>

Signed-off-by: Pavithra Barithaya <pavithra.b@ibm.com>

host-bmc/host_pdr_handler.cpp
@@ -458,13 +458,21 @@ int HostPDRHandler::handleStateSensorEvent(
     return PLDM_SUCCESS;
 }
 
-void HostPDRHandler::mergeEntityAssociations(const std::vector<uint8_t>& pdr)
+void HostPDRHandler::mergeEntityAssociations(
+    const std::vector<uint8_t>& pdr, [[maybe_unused]] const uint32_t& size,
+    [[maybe_unused]] const uint32_t& record_handle)
 {
     size_t numEntities{};
     pldm_entity* entities = nullptr;
     bool merged = false;
     auto entityPdr = reinterpret_cast<pldm_pdr_entity_association*>(
         const_cast<uint8_t*>(pdr.data()) + sizeof(pldm_pdr_hdr));
+
+    if (oemPlatformHandler && oemPlatformHandler->isHBRange(record_handle))
+    {
+        // Adding the HostBoot range PDRs to the repo before merging it
+        pldm_pdr_add(repo, pdr.data(), size, record_handle, true, 0xFFFF);
+    }
 
     pldm_entity_association_pdr_extract(pdr.data(), pdr.size(), &numEntities,
                                         &entities);
@@ -764,7 +772,7 @@ void HostPDRHandler::processHostPDRs(mctp_eid_t /*eid*/,
 
             if (pdrHdr->type == PLDM_PDR_ENTITY_ASSOCIATION)
             {
-                this->mergeEntityAssociations(pdr);
+                this->mergeEntityAssociations(pdr, respCount, rh);
                 merged = true;
             }
             else

libpldmresponder/oem_handler.hpp
@@ -156,6 +156,14 @@ class Handler : public CmdHandler
      */
     virtual void setSurvTimer(uint8_t tid, bool value) = 0;
 
+    /** @brief To check if record handle is in HostBoot range
+     *
+     *  @param[in] record_handle - record handle of the PDR
+     *
+     *  @return true or false
+     */
+    virtual bool isHBRange(const uint32_t& record_handle) = 0;
+
     virtual ~Handler() = default;
 
   protected:

oem/ibm/libpldmresponder/oem_ibm_handler.cpp
@@ -1923,6 +1923,16 @@ void pldm::responder::oem_ibm_platform::Handler::triggerHostEffecter(
     }
 }
 
+bool pldm::responder::oem_ibm_platform::Handler::isHBRange(
+    const uint32_t& record_handle)
+{
+    if (record_handle >= 0x01000000 && record_handle < 0x01FFFFFF)
+    {
+        return true;
+    }
+    return false;
+}
+
 } // namespace oem_ibm_platform
 } // namespace responder
 } // namespace pldm

oem/ibm/libpldmresponder/oem_ibm_handler.hpp
@@ -471,6 +471,13 @@ class Handler : public oem_platform::Handler
      */
     void triggerHostEffecter(bool value, std::string path);
 
+    /** @brief method to check if the record handle is within the HostBoot range
+     *  or not
+     *
+     *  @param[in] record_handle - record handle of the pdr
+     */
+    bool isHBRange(const uint32_t& record_handle);
+
     ~Handler() = default;
 
     pldm::responder::CodeUpdate* codeUpdate; //!< pointer to CodeUpdate object

host-bmc/host_pdr_handler.hpp
@@ -224,8 +224,13 @@ class HostPDRHandler
      *  @details A merge operation involves adding a pldm_entity under the
      *  appropriate parent, and updating container ids.
      *  @param[in] pdr - entity association pdr
+     *  @param[in] size - size of input PDR record in bytes
+     *  @param[in] record_handle - record handle of the PDR
      */
-    void mergeEntityAssociations(const std::vector<uint8_t>& pdr);
+    void
+        mergeEntityAssociations(const std::vector<uint8_t>& pdr,
+                                [[maybe_unused]] const uint32_t& size,
+                                [[maybe_unused]] const uint32_t& record_handle);
 
     /** @brief process the Host's PDR and add to BMC's PDR repo
      *  @param[in] eid - MCTP id of Host

commit e646ec696ce29ad61ba0298fafceadc8d48b1299

Fix the wrong container id in SBE numeric effecter
This commit solves the issue caused by #319 where we
persisted the HB entity association PDRs before we normalise
them. This lead to assigning the wrong container ids for
the SBE numeric effecter PDRs created.
With this fix, we assign the normalised container ids to the
numeric effecter PDRs.

This commit also removes the stale code to update the container
ids of proc LEDs. The JSON file is already updated with the
correct conatiner id and this piece of code to update it is
not needed.

Fixes: PE00DXJZ

Signed-off-by: Pavithra Barithaya <pavithra.b@ibm.com>

host-bmc/host_pdr_handler.cpp
@@ -5,7 +5,10 @@
 #include "libpldm/fru.h"
 #include "libpldm/requester/pldm.h"
 #include "libpldm/state_set.h"
+#ifdef OEM_IBM
 #include "oem/ibm/libpldm/fru.h"
+#include "oem/ibm/libpldm/pdr_oem_ibm.h"
+#endif
 
 #include "dbus/custom_dbus.hpp"
 #include "dbus/serialize.hpp"
@@ -468,11 +471,13 @@ void HostPDRHandler::mergeEntityAssociations(
     auto entityPdr = reinterpret_cast<pldm_pdr_entity_association*>(
         const_cast<uint8_t*>(pdr.data()) + sizeof(pldm_pdr_hdr));
 
-    if (oemPlatformHandler && oemPlatformHandler->isHBRange(record_handle))
+#ifdef OEM_IBM
+    if (oemPlatformHandler && isHBRange(record_handle))
     {
         // Adding the HostBoot range PDRs to the repo before merging it
         pldm_pdr_add(repo, pdr.data(), size, record_handle, true, 0xFFFF);
     }
+#endif
 
     pldm_entity_association_pdr_extract(pdr.data(), pdr.size(), &numEntities,
                                         &entities);
@@ -996,7 +1001,6 @@ void HostPDRHandler::_processPDRRepoChgEvent(
     if (oemPlatformHandler != nullptr)
     {
         oemPlatformHandler->updateContainerID();
-        oemPlatformHandler->updateContainerIDofProcLed();
     }
     deferredPDRRepoChgEvent.reset();
     this->sendPDRRepositoryChgEvent(
@@ -1604,6 +1608,7 @@ void HostPDRHandler::setLocationCode(
                 continue;
             }
 
+#ifdef OEM_IBM
             if (data.fruRecType == PLDM_FRU_RECORD_TYPE_OEM)
             {
                 for (auto& tlv : data.fruTLV)
@@ -1619,6 +1624,7 @@ void HostPDRHandler::setLocationCode(
                     }
                 }
             }
+#endif
             else
             {
                 for (auto& tlv : data.fruTLV)

libpldm/pdr.c
@@ -623,37 +623,6 @@ pldm_entity pldm_get_entity_from_record_handle(const pldm_pdr *repo,
 	return element;
 }
 
-uint16_t pldm_find_container_id(const pldm_pdr *repo, uint16_t entityType,
-				uint16_t entityInstance)
-{
-	assert(repo != NULL);
-
-	pldm_pdr_record *record = repo->first;
-
-	while (record != NULL) {
-		struct pldm_pdr_hdr *hdr = (struct pldm_pdr_hdr *)record->data;
-		if (hdr->type == PLDM_PDR_ENTITY_ASSOCIATION) {
-			struct pldm_pdr_entity_association *pdr =
-			    (struct pldm_pdr_entity_association
-				 *)((uint8_t *)record->data +
-				    sizeof(struct pldm_pdr_hdr));
-			struct pldm_entity *child =
-			    (struct pldm_entity *)(&pdr->children[0]);
-			for (int i = 0; i < pdr->num_children; ++i) {
-				if (pdr->container.entity_type == entityType &&
-				    pdr->container.entity_instance_num ==
-					entityInstance) {
-					uint16_t id =
-					    child->entity_container_id;
-					return id;
-				}
-			}
-		}
-		record = record->next;
-	}
-	return 0;
-}
-
 void pldm_change_container_id_of_effecter(const pldm_pdr *repo,
 					  uint16_t effecterId,
 					  uint16_t containerId)

libpldm/pdr.h
@@ -197,15 +197,6 @@ void pldm_pdr_update_TL_pdr(const pldm_pdr *repo, uint16_t terminusHandle,
 void pldm_delete_by_record_handle(pldm_pdr *repo, uint32_t record_handle,
 				  bool is_remote);
 
-/** @brief find the container ID of the contained entity
- *
- *  @param[in] repo - opaque pointer acting as a PDR repo handle
- *  @param[in] entityType - entity type
- *  @param[in] entityInstance - instance of the entity
- */
-uint16_t pldm_find_container_id(const pldm_pdr *repo, uint16_t entityType,
-				uint16_t entityInstance);
-
 /** @brief update the container id of an effecter
  *
  *  @param[in] repo -  opaque pointer acting as a PDR repo handle

libpldmresponder/oem_handler.hpp
@@ -140,9 +140,6 @@ class Handler : public CmdHandler
     /** @brief To handle the boot types bios attributes at shutdown*/
     virtual void handleBootTypesAtChassisOff() = 0;
 
-    /** @brief To update container ID of Proc LED PDRs */
-    virtual void updateContainerIDofProcLed() = 0;
-
     /** @brief Interface to reset or stop the surveillance timer
      *  @param[in] value - true or false, to indicate if the timer
      *                     should be reset or turned off*/
@@ -155,14 +152,6 @@ class Handler : public CmdHandler
      *                    running or not
      */
     virtual void setSurvTimer(uint8_t tid, bool value) = 0;
-
-    /** @brief To check if record handle is in HostBoot range
-     *
-     *  @param[in] record_handle - record handle of the PDR
-     *
-     *  @return true or false
-     */
-    virtual bool isHBRange(const uint32_t& record_handle) = 0;
 
     virtual ~Handler() = default;
 

oem/ibm/libpldm/pdr_oem_ibm.c
@@ -27,3 +27,43 @@ pldm_pdr_record *pldm_pdr_find_last_local_record(const pldm_pdr *repo)
 	}
 	return NULL;
 }
+
+bool isHBRange(const uint32_t record_handle)
+{
+	if (record_handle >= 0x01000000 && record_handle < 0x01FFFFFF) {
+		return true;
+	}
+	return false;
+}
+
+uint16_t pldm_find_container_id(const pldm_pdr *repo, uint16_t entityType,
+				uint16_t entityInstance)
+{
+	assert(repo != NULL);
+
+	pldm_pdr_record *record = repo->first;
+
+	while (record != NULL) {
+		struct pldm_pdr_hdr *hdr = (struct pldm_pdr_hdr *)record->data;
+		if (hdr->type == PLDM_PDR_ENTITY_ASSOCIATION &&
+		    !(isHBRange(record->record_handle))) {
+			struct pldm_pdr_entity_association *pdr =
+			    (struct pldm_pdr_entity_association
+				 *)((uint8_t *)record->data +
+				    sizeof(struct pldm_pdr_hdr));
+			struct pldm_entity *child =
+			    (struct pldm_entity *)(&pdr->children[0]);
+			for (int i = 0; i < pdr->num_children; ++i) {
+				if (pdr->container.entity_type == entityType &&
+				    pdr->container.entity_instance_num ==
+					entityInstance) {
+					uint16_t id =
+					    child->entity_container_id;
+					return id;
+				}
+			}
+		}
+		record = record->next;
+	}
+	return 0;
+}

oem/ibm/libpldm/pdr_oem_ibm.h
@@ -6,6 +6,7 @@ extern "C" {
 #endif
 
 #include "pdr_data.h"
+#include "platform.h"
 #include <assert.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -19,6 +20,21 @@ extern "C" {
  */
 pldm_pdr_record *pldm_pdr_find_last_local_record(const pldm_pdr *repo);
 
+/** @brief method to check if the record handle is within the HostBoot range
+ *  or not
+ *
+ *  @param[in] record_handle - record handle of the pdr
+ */
+bool isHBRange(const uint32_t record_handle);
+
+/** @brief find the container ID of the contained entity
+ *
+ *  @param[in] repo - opaque pointer acting as a PDR repo handle
+ *  @param[in] entityType - entity type
+ *  @param[in] entityInstance - instance of the entity
+ */
+uint16_t pldm_find_container_id(const pldm_pdr *repo, uint16_t entityType,
+				uint16_t entityInstance);
 #ifdef __cplusplus
 }
 #endif

oem/ibm/libpldmresponder/oem_ibm_handler.cpp
@@ -1576,129 +1576,6 @@ uint8_t pldm::responder::oem_ibm_platform::Handler::fetchRealSAIStatus()
 }
 
 void pldm::responder::oem_ibm_platform::Handler::
-    updateIdentifyEffecterAndSensorPDRs()
-{
-    pldm::pdr::EntityType entityType = PLDM_ENTITY_PROC;
-
-    auto stateEffecterPDRs = pldm::utils::findStateEffecterPDR(
-        mctp_eid, entityType,
-        static_cast<uint16_t>(PLDM_STATE_SET_IDENTIFY_STATE), pdrRepo);
-
-    if (stateEffecterPDRs.empty())
-    {
-        std::cerr << "The State effecter PDR cannot be found, entityType = "
-                  << entityType << std::endl;
-        return;
-    }
-
-    for (auto& effecterPDR : stateEffecterPDRs)
-    {
-        for (auto& [key, value] : instanceMap)
-        {
-            auto instNumber = (value.dcmId * 2) + value.procId;
-            auto pdr =
-                reinterpret_cast<pldm_state_effecter_pdr*>(effecterPDR.data());
-            if (instNumber == ((pdr->entity_instance) - 1))
-            {
-                uint16_t newContainerID = pldm_find_container_id(
-                    pdrRepo, PLDM_ENTITY_PROC_MODULE, value.dcmId);
-                pldm_change_container_id_of_effecter(pdrRepo, pdr->effecter_id,
-                                                     newContainerID);
-                pldm_change_instance_number_of_effecter(
-                    pdrRepo, pdr->effecter_id, (instNumber % 2));
-                break;
-            }
-        }
-    }
-
-    auto stateSensorIdentifyPDRs = findStateSensorPDR(
-        mctp_eid, entityType,
-        static_cast<uint16_t>(PLDM_STATE_SET_IDENTIFY_STATE), pdrRepo);
-    for (auto& identifySensorPDR : stateSensorIdentifyPDRs)
-    {
-        for (auto& [key, value] : instanceMap)
-        {
-            auto instanceNumber = (value.dcmId * 2) + value.procId;
-            auto pdr = reinterpret_cast<pldm_state_sensor_pdr*>(
-                identifySensorPDR.data());
-            if (instanceNumber == ((pdr->entity_instance) - 1))
-            {
-                uint16_t newContainerID = pldm_find_container_id(
-                    pdrRepo, PLDM_ENTITY_PROC_MODULE, value.dcmId);
-                pldm_change_container_id_of_sensor(pdrRepo, pdr->sensor_id,
-                                                   newContainerID);
-                pldm_change_instance_number_of_sensor(pdrRepo, pdr->sensor_id,
-                                                      (instanceNumber % 2));
-                break;
-            }
-        }
-    }
-}
-
-void pldm::responder::oem_ibm_platform::Handler::
-    updateFaultEffecterAndSensorPDRs()
-{
-    pldm::pdr::EntityType entityType = PLDM_ENTITY_PROC;
-    auto stateEffecterFaultPDRs = pldm::utils::findStateEffecterPDR(
-        mctp_eid, entityType,
-        static_cast<uint16_t>(PLDM_STATE_SET_OPERATIONAL_FAULT_STATUS),
-        pdrRepo);
-    for (auto& faultEffecterPDR : stateEffecterFaultPDRs)
-    {
-        for (auto& [key, value] : instanceMap)
-        {
-            auto instanceNumber = (value.dcmId * 2) + value.procId;
-            auto pdr = reinterpret_cast<pldm_state_effecter_pdr*>(
-                faultEffecterPDR.data());
-            if (instanceNumber == (pdr->entity_instance) - 1)
-            {
-                uint16_t newContainerID = pldm_find_container_id(
-                    pdrRepo, PLDM_ENTITY_PROC_MODULE, value.dcmId);
-                pldm_change_container_id_of_effecter(pdrRepo, pdr->effecter_id,
-                                                     newContainerID);
-                pldm_change_instance_number_of_effecter(
-                    pdrRepo, pdr->effecter_id, (instanceNumber % 2));
-                break;
-            }
-        }
-    }
-
-    auto stateSensorFaultPDRs = findStateSensorPDR(
-        mctp_eid, entityType,
-        static_cast<uint16_t>(PLDM_STATE_SET_OPERATIONAL_FAULT_STATUS),
-        pdrRepo);
-    for (auto& faultSensorPDR : stateSensorFaultPDRs)
-    {
-        for (auto& [key, value] : instanceMap)
-        {
-            auto instanceNumber = (value.dcmId * 2) + value.procId;
-            auto pdr =
-                reinterpret_cast<pldm_state_sensor_pdr*>(faultSensorPDR.data());
-            if (instanceNumber == (pdr->entity_instance) - 1)
-            {
-                uint16_t newContainerID = pldm_find_container_id(
-                    pdrRepo, PLDM_ENTITY_PROC_MODULE, value.dcmId);
-                pldm_change_container_id_of_sensor(pdrRepo, pdr->sensor_id,
-                                                   newContainerID);
-                pldm_change_instance_number_of_sensor(pdrRepo, pdr->sensor_id,
-                                                      (instanceNumber % 2));
-                break;
-            }
-        }
-    }
-}
-
-void pldm::responder::oem_ibm_platform::Handler::updateContainerIDofProcLed()
-{
-    if (update)
-    {
-        updateIdentifyEffecterAndSensorPDRs();
-        updateFaultEffecterAndSensorPDRs();
-        update = false;
-    }
-}
-
-void pldm::responder::oem_ibm_platform::Handler::
     processPowerCycleOffSoftGraceful()
 {
     std::cerr << "Received soft graceful power cycle request" << std::endl;
@@ -1922,17 +1799,6 @@ void pldm::responder::oem_ibm_platform::Handler::triggerHostEffecter(
         }
     }
 }
-
-bool pldm::responder::oem_ibm_platform::Handler::isHBRange(
-    const uint32_t& record_handle)
-{
-    if (record_handle >= 0x01000000 && record_handle < 0x01FFFFFF)
-    {
-        return true;
-    }
-    return false;
-}
-
 } // namespace oem_ibm_platform
 } // namespace responder
 } // namespace pldm

oem/ibm/libpldmresponder/oem_ibm_handler.hpp
@@ -422,15 +422,6 @@ class Handler : public oem_platform::Handler
      */
     uint8_t fetchRealSAIStatus();
 
-    /** @brief updating the container ID of PROC LEd PDRs*/
-    void updateContainerIDofProcLed();
-
-    /** @brief update conatiner id of identify effecter and sensor LED PDRs*/
-    void updateIdentifyEffecterAndSensorPDRs();
-
-    /** @brief update conatiner id of fault effecter and sensor LED PDRs*/
-    void updateFaultEffecterAndSensorPDRs();
-
     /** @brief To process the graceful shutdown, cycle chassis power, and boot
      *  the host back up*/
     void processPowerCycleOffSoftGraceful();
@@ -470,13 +461,6 @@ class Handler : public oem_platform::Handler
      *  @param[in] path - path for which the effecter is set
      */
     void triggerHostEffecter(bool value, std::string path);
-
-    /** @brief method to check if the record handle is within the HostBoot range
-     *  or not
-     *
-     *  @param[in] record_handle - record handle of the pdr
-     */
-    bool isHBRange(const uint32_t& record_handle);
 
     ~Handler() = default;
 