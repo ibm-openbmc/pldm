20c4b2dbf0ce06e47761d060a715124c1355972a
Varsha Kaverappa <vkaverap@in.ibm.com>
2022-12-05 00:34:30 -0600

    Support New record to the backplane vpd for hostboot to consume
    This commit is to support a new VPD record called PSPD that contains
    few keywords called #D and VM. Since these keywords are typically
    larger in size (4K bytes) than what is managed by PLDM, we are adding
    a file into which these keywords will be extracted. This keyword file
    is transferred to the HB PLDM layer with a readFileByType request
    from HB.
    
    Signed-off-by: Varsha Kaverappa <vkaverap@in.ibm.com>

--- oem/ibm/libpldmresponder/file_io_by_type.cpp
+++ oem/ibm/libpldmresponder/file_io_by_type.cpp
@@ -13,6 +13,7 @@
 #include "file_io_type_pcie.hpp"
 #include "file_io_type_pel.hpp"
 #include "file_io_type_progress_src.hpp"
+#include "file_io_type_vpd.hpp"
 #include "xyz/openbmc_project/Common/error.hpp"
 
 #include <stdint.h>
@@ -181,6 +182,10 @@ std::unique_ptr<FileHandler> getHandlerByType(uint16_t fileType,
         case PLDM_FILE_TYPE_CABLE_INFO:
         {
             return std::make_unique<PCIeInfoHandler>(fileHandle, fileType);
+        }
+        case PLDM_FILE_TYPE_VPD_KEYWORD:
+        {
+            return std::make_unique<keywordHandler>(fileHandle, fileType);
         }
         default:
         {
