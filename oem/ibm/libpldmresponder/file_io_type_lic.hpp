1c704ae15805e5e5ff5b6af4564daa78bb9da80c
Kamalkumar Patel <kamalkumar.patel@ibm.com>
2024-04-29 03:35:51 -0500

    Fixing Read lid size corruption issue
    When HOST read/write data to BMC using DMA then BMC checks the length
    of the data to be read/write and divide into chunks if data length is
    more than DMA max size.
    This change will fix the issue in PLDM when it send data transfer
    status to the HOST and sending left over length as response which is
    wrong and now PLDM sends correct response with length.
    
    Signed-off-by: Kamalkumar Patel <kamalkumar.patel@ibm.com>

--- oem/ibm/libpldmresponder/file_io_type_lic.hpp
+++ oem/ibm/libpldmresponder/file_io_type_lic.hpp
@@ -27,7 +27,7 @@ class LicenseHandler : public FileHandler
                                 uint64_t address,
                                 oem_platform::Handler* /*oemPlatformHandler*/);
 
-    virtual int readIntoMemory(uint32_t /*offset*/, uint32_t& /*length*/,
+    virtual int readIntoMemory(uint32_t /*offset*/, uint32_t /*length*/,
                                uint64_t /*address*/,
                                oem_platform::Handler* /*oemPlatformHandler*/)
     {
