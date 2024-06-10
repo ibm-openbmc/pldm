355c5695ff0e2f01929f06f771cd5a6819a627da
Jayashankar Padath <jayashankar.padath@in.ibm.com>
2024-06-10 03:50:03 -0500

    oem-ibm: Capacity on demand licensing support
    This commit supports:
    
    1. Creation or update of new license interfaces and properties supplied by host
    2. Clearing of license status to Unknown, during host power off transition
    3. Restoration of license interfaces during reset reload and setting the
       license status to Unknown
    
    Tested By:
    
    1. Verified that dbus hosting is done by pldm
    2. CoD(Capacity on demand) values are coming up in the BMC
    3. Verified that reboot of BMC does not clear values
    
    Change-Id: I9377e66753c2df7f8e1ac8b6a4ed285db69ad090
    Signed-off-by: Jayashankar Padath <jayashankar.padath@in.ibm.com>

--- host-bmc/custom_dbus.cpp
+++ host-bmc/custom_dbus.cpp
@@ -27,5 +27,69 @@ std::optional<std::string>
     return std::nullopt;
 }
 
+void CustomDBus::setOperationalStatus(const std::string& path, uint8_t status)
+{
+    if (!operationalStatus.contains(path))
+    {
+        operationalStatus.emplace(
+            path, std::make_unique<OperationalStatusIntf>(
+                      pldm::utils::DBusHandler::getBus(), path.c_str()));
+    }
+
+    if (status == PLDM_STATE_SET_OPERATIONAL_STRESS_STATUS_NORMAL)
+    {
+        operationalStatus.at(path)->functional(true);
+    }
+    else
+    {
+        operationalStatus.at(path)->functional(false);
+    }
+}
+
+bool CustomDBus::getOperationalStatus(const std::string& path) const
+{
+    if (operationalStatus.contains(path))
+    {
+        return operationalStatus.at(path)->functional();
+    }
+
+    return false;
+}
+
+void CustomDBus::implementLicInterfaces(
+    const std::string& path, const uint32_t& authdevno, const std::string& name,
+    const std::string& serialno, const uint64_t& exptime,
+    const sdbusplus::com::ibm::License::Entry::server::LicenseEntry::Type& type,
+    const sdbusplus::com::ibm::License::Entry::server::LicenseEntry::
+        AuthorizationType& authtype)
+{
+    if (!codLic.contains(path))
+    {
+        codLic.emplace(
+            path, std::make_unique<LicIntf>(pldm::utils::DBusHandler::getBus(),
+                                            path.c_str()));
+    }
+
+    codLic.at(path)->authDeviceNumber(authdevno);
+    codLic.at(path)->name(name);
+    codLic.at(path)->serialNumber(serialno);
+    codLic.at(path)->expirationTime(exptime);
+    codLic.at(path)->type(type);
+    codLic.at(path)->authorizationType(authtype);
+}
+
+void CustomDBus::setAvailabilityState(const std::string& path,
+                                      const bool& state)
+{
+    if (!availabilityState.contains(path))
+    {
+        availabilityState.emplace(
+            path, std::make_unique<AvailabilityIntf>(
+                      pldm::utils::DBusHandler::getBus(), path.c_str()));
+    }
+
+    availabilityState.at(path)->available(state);
+}
+
 } // namespace dbus
 } // namespace pldm
