339b795d2084a81c833a423184a883f35cf7c3c8
Archana Kakani <archana.kakani@ibm.com>
2024-07-11 03:36:35 -0500

    pldm: Add support of FRU associations
    This commit adds support of dynamic assoication.
    Corresponding 1020 commit
    https://github.com/ibm-openbmc/pldm/commit/3ec58989c9cd1ad37dff25c4dc439e86866e6f09
    
    Signed-off-by: Archana Kakani <archana.kakani@ibm.com>

--- host-bmc/host_pdr_handler.cpp
+++ host-bmc/host_pdr_handler.cpp
@@ -131,6 +131,9 @@ HostPDRHandler::HostPDRHandler(
                     auto const& [key, value] = item;
                     return key != TERMINUS_HANDLE;
                 });
+                // when the host is powered off, set the availability
+                // state of all the dbus objects to false
+                this->setPresenceFrus();
                 pldm_pdr_remove_remote_pdrs(repo);
                 pldm_entity_association_tree_destroy_root(entityTree);
                 pldm_entity_association_tree_copy_root(bmcEntityTree,
@@ -169,6 +172,15 @@ HostPDRHandler::HostPDRHandler(
             }
         }
     });
+}
+
+void HostPDRHandler::setPresenceFrus()
+{
+    // iterate over all dbus objects
+    for (const auto& [path, entityId] : objPathMap)
+    {
+        CustomDBus::getCustomDBus().setAvailabilityState(path, false);
+    }
 }
 
 void HostPDRHandler::fetchPDR(PDRRecordHandles&& recordHandles, uint8_t tid)
@@ -1626,11 +1638,52 @@ void HostPDRHandler::createDbusObjects()
         }
     }
     getFRURecordTableMetadataByRemote();
-    //this->setFRUDynamicAssociations();
+    this->setFRUDynamicAssociations();
 
     // update xyz.openbmc_project.State.Decorator.OperationalStatus
     setOperationStatus();
     error("Refreshing dbus hosted by pldm Completed");
+}
+
+void HostPDRHandler::setFRUDynamicAssociations()
+{
+    for (const auto& [leftPath, leftEntity] : objPathMap)
+    {
+        // for each path, compare it with rest of the paths in the
+        // map
+        uint16_t leftEntityType = leftEntity.entity_type;
+        for (const auto& [rightPath, rightEntity] : objPathMap)
+        {
+            uint16_t rightEntityType = rightEntity.entity_type;
+            // if leftpath is same as rightPath
+            // then both dbus objects are same, so
+            // no need to create any associations
+            if (leftPath == rightPath)
+            {
+                continue;
+            }
+            else if ((rightPath.string().find(leftPath) != std::string::npos) ||
+                     (leftPath.string().find(rightPath) != std::string::npos))
+            {
+                // this means left path dbus object is parent of the
+                // right path dbus object, something like this
+                // leftpath = /xyz/openbmc_project/system/chassis15363
+                // rightpath = /xyz/openbmc_project/system/chassis15363/fan1
+                auto key = std::make_pair(leftEntityType, rightEntityType);
+                if (associationsParser->associationsInfoMap.contains(key))
+                {
+                    auto value = associationsParser->associationsInfoMap[key];
+                    // we have some associations defined for this parent type &
+                    // child type
+                    std::vector<
+                        std::tuple<std::string, std::string, std::string>>
+                        associations{{value.first, value.second, rightPath}};
+                    CustomDBus::getCustomDBus().setAssociations(leftPath,
+                                                                associations);
+                }
+            }
+        }
+    }
 }
 
 void HostPDRHandler::setOperationStatus()
@@ -1739,6 +1792,35 @@ std::string HostPDRHandler::getParentChassis(const std::string& frupath)
     return "";
 }
 
+void HostPDRHandler::setRecordPresent(uint32_t recordHandle)
+{
+    pldm_entity recordEntity = pldm_get_entity_from_record_handle(repo,
+                                                                  recordHandle);
+    for (const auto& [path, dbusEntity] : objPathMap)
+    {
+        if (dbusEntity.entity_type == recordEntity.entity_type &&
+            dbusEntity.entity_instance_num ==
+                recordEntity.entity_instance_num &&
+            dbusEntity.entity_container_id == recordEntity.entity_container_id)
+        {
+            error(
+                "Removing Host FRU [ {PATH} ] with entityid [ {ENTITY_TYP}, {ENTITY_NUM}, {ENTITY_ID} ]",
+                "PATH", path, "ENTITY_TYP", (unsigned)recordEntity.entity_type,
+                "ENTITY_NUM", (unsigned)recordEntity.entity_instance_num,
+                "ENTITY_ID", (unsigned)recordEntity.entity_container_id);
+            // if the record has the same entity id, mark that dbus object as
+            // not present
+            CustomDBus::getCustomDBus().updateItemPresentStatus(path, false);
+            CustomDBus::getCustomDBus().setOperationalStatus(
+                path, false, getParentChassis(path));
+            // Delete the LED object path
+            auto ledGroupPath = updateLedGroupPath(path);
+            pldm::dbus::CustomDBus::getCustomDBus().deleteObject(ledGroupPath);
+            return;
+        }
+    }
+}
+
 std::string HostPDRHandler::updateLedGroupPath(const std::string& path)
 {
     std::string ledGroupPath{};
@@ -1758,8 +1840,8 @@ void HostPDRHandler::deletePDRFromRepo(PDRRecordHandles&& recordHandles)
     {
         error("Record handle deleted: {REC_HANDLE}", "REC_HANDLE",
               recordHandle);
-        //this->setRecordPresent(recordHandle);
-        //pldm_delete_by_record_handle(repo, recordHandle, true);
+        this->setRecordPresent(recordHandle);
+        pldm_delete_by_record_handle(repo, recordHandle, true);
     }
 }
 
