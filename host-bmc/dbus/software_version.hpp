eb4a2eaeb89fc239d3b3107560633dfcd0164b04
Archana Kakani <archana.kakani@ibm.com>
2024-07-22 04:04:08 -0500

    Support for Mex IO Enclosure version
    PHYP sends a FRU record set PDR and send the io enclosure
    version data via the frurecord table, and pldm parses that
    and hosts the Version Interface on the mex chassis object.
    
    Currently phyp does not have the support for this yet, so
    hardcoded a dummy value for now, would have to remove that part
    of code once the phyp change is in the driver.
    
    https://github.com/ibm-openbmc/pldm/commit/8ca3014a6d7de340c587f28f00f1c99cf3b746d2
    
    Signed-off-by: Archana Kakani <archana.kakani@ibm.com>

---
+++ host-bmc/dbus/software_version.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/server.hpp>
+#include <sdbusplus/server/object.hpp>
+#include <xyz/openbmc_project/Software/Version/server.hpp>
+
+#include <string>
+
+namespace pldm
+{
+namespace dbus
+{
+using SoftWareVersionIntf = sdbusplus::server::object::object<
+    sdbusplus::xyz::openbmc_project::Software::server::Version>;
+
+class SoftWareVersion : public SoftWareVersionIntf
+{
+  public:
+    SoftWareVersion() = delete;
+    ~SoftWareVersion() = default;
+    SoftWareVersion(const SoftWareVersion&) = delete;
+    SoftWareVersion& operator=(const SoftWareVersion&) = delete;
+    SoftWareVersion(SoftWareVersion&&) = default;
+    SoftWareVersion& operator=(SoftWareVersion&&) = default;
+
+    SoftWareVersion(sdbusplus::bus_t& bus, const std::string& objPath) :
+        SoftWareVersionIntf(bus, objPath.c_str()), path(objPath)
+    {}
+
+    /** Get value of Version */
+    std::string version() const override;
+
+    /** Set value of Version */
+    std::string version(std::string value) override;
+
+    /** Get value of Purpose */
+    VersionPurpose purpose() const override;
+
+    /** Set value of Purpose */
+    VersionPurpose purpose(VersionPurpose value) override;
+
+  private:
+    std::string path;
+
+};
+
+} // namespace dbus
+} // namespace pldm
