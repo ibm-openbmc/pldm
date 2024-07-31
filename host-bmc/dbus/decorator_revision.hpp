c1bd9e15a02cf70bc4e453b49551437bfc07afdd
Archana Kakani <archana.kakani@ibm.com>
2024-07-31 09:27:10 -0500

    host-bmc: Use Decorator.Revision for enclosure version
    Bmcweb changed the Enclosure Firmware version to use the standard Redfish
    schema like
    $ curl -k -X GET https://${bmc}/redfish/v1/Chassis/chassis15363
    ...
    "Oem": {
       "OpenBMC": {
       "FirmwareVersion": "03MEX200924a"
    }
    to
    $ curl -k -X GET https://${bmc}/redfish/v1/Chassis/chassis15363
      ...
      "Version": "03MEX200924a"
    
    Changing PLDM to use the interface "xyz.openbmc_project.Inventory.Decorator.Revision"
    (instead of xyz.openbmc_project.Software.Version) to provide the enclosure version.
    
    Tested: Poweron/PowerOff
    
    Signed-off-by: Archana Kakani <archana.kakani@ibm.com>

---
+++ host-bmc/dbus/decorator_revision.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/server.hpp>
+#include <sdbusplus/server/object.hpp>
+#include <xyz/openbmc_project/Inventory/Decorator/Revision/server.hpp>
+
+#include <string>
+
+namespace pldm
+{
+namespace dbus
+{
+using DecoratorRevisionIntf = sdbusplus::server::object::object<
+    sdbusplus::xyz::openbmc_project::Inventory::Decorator::server::Revision>;
+
+class DecoratorRevision : public DecoratorRevisionIntf
+{
+  public:
+    DecoratorRevision() = delete;
+    ~DecoratorRevision() = default;
+    DecoratorRevision(const DecoratorRevision&) = delete;
+    DecoratorRevision& operator=(const DecoratorRevision&) = delete;
+    DecoratorRevision(DecoratorRevision&&) = default;
+    DecoratorRevision& operator=(DecoratorRevision&&) = default;
+
+    DecoratorRevision(sdbusplus::bus_t& bus, const std::string& objPath) :
+        DecoratorRevisionIntf(bus, objPath.c_str()), path(objPath)
+    {}
+
+    /** Get value of Version */
+    std::string version() const override;
+
+    /** Set value of Version */
+    std::string version(std::string value) override;
+
+  private:
+    std::string path;
+};
+
+} // namespace dbus
+} // namespace pldm
