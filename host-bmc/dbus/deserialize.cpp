fb36ce7e80aa1fdaaead6547a83f06ba5733b9e1
Archana Kakani <archana.kakani@ibm.com>
2024-08-01 09:46:35 -0500

    pldm: Deserialize support for Licence
    Signed-off-by: Archana Kakani <archana.kakani@ibm.com>

--- host-bmc/dbus/deserialize.cpp
+++ host-bmc/dbus/deserialize.cpp
@@ -133,6 +133,44 @@ std::unordered_map<std::string, callback> dBusInterfaceHandler{
      [](const std::string& path, Properties /* values */) {
     pldm::dbus::CustomDBus::getCustomDBus().implementGlobalInterface(path);
 }},
+    {"LicenseEntry",
+     [](const std::string& path, Properties values) {
+    std::string name{};
+    std::string serialno{};
+    dbus::LicenseEntryType type;
+    dbus::LicenseEntryAuthorizationType authtype;
+    uint32_t authdevno{};
+    uint64_t exptime{};
+
+    if (values.contains("name"))
+    {
+        name = std::get<std::string>(values.at("name"));
+    }
+    if (values.contains("serialNumber"))
+    {
+        serialno = std::get<std::string>(values.at("serialNumber"));
+    }
+    if (values.contains("type"))
+    {
+        type = std::get<dbus::LicenseEntryType>(values.at("type"));
+    }
+    if (values.contains("authorizationType"))
+    {
+        authtype = std::get<dbus::LicenseEntryAuthorizationType>(
+            values.at("authorizationType"));
+    }
+    if (values.contains("expirationTime"))
+    {
+        exptime = std::get<uint64_t>(values.at("expirationTime"));
+    }
+    if (values.contains("authDeviceNumber"))
+    {
+        authdevno = std::get<uint32_t>(values.at("authDeviceNumber"));
+    }
+
+    pldm::dbus::CustomDBus::getCustomDBus().implementLicInterfaces(
+        path, authdevno, name, serialno, exptime, type, authtype);
+}},
     {"SoftWareVersion",
      [](const std::string& path, Properties values) {
     std::string version{};
