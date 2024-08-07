221f4396e6e12f3b7e959f6a37dd856996514053
Pavithra B <46603473+Pavithrab7@users.noreply.github.com>
2024-08-07 19:01:51 +0530

    Move the oemUtilsHandler declaration out of oem-ibm (#586)
    The oemUtilsHandler was declared in oem-ibm scope and accessed
    outside oem-ibm scope which would cause a problem when pldm was
    compiled with oem-ibm disabled.
    
    Signed-off-by: Pavithra Barithaya <pavithrabarithaya07@gmail.com>

--- pldmd/pldmd.cpp
+++ pldmd/pldmd.cpp
@@ -251,6 +251,8 @@ int main(int argc, char** argv)
     std::unique_ptr<platform_config::Handler> platformConfigHandler{};
     platformConfigHandler =
         std::make_unique<platform_config::Handler>(PDR_JSONS_DIR);
+    std::unique_ptr<oem_fru::Handler> oemFruHandler{};
+    std::unique_ptr<oem_utils::Handler> oemUtilsHandler{};
 
     if (hostEID)
     {
